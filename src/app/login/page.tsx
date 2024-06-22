cbb9ea30427de52f88b2f6ae40c314bb2bd05a50
1carlosrenan <carlosrenan96@gmail.com>
2024-06-22 08:38:58 -0300

    feat: login form and action

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,10 @@
-export default async function LoginPage(){
-  return <main><h1>Login</h1></main>
+import LoginForm from '@/components/login/login-form';
+
+export default async function LoginPage() {
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <LoginForm />
+    </section>
+  );
 }
